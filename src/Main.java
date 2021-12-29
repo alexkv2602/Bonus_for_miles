cd02056a3afe624cf43784a5b2af58f35e6d762f
Alex Kleschenko <alexkv2694@gmail.com>
2021-12-29 22:08:31 +0300

    add Main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 10000;
+        int bonus = 20;
+        int miles = price / bonus;
+
+        System.out.println(miles);
+    }
+}
