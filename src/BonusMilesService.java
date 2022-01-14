04d33251e663b022dad5dfdc1bee2b6b95a514ee
Alex Kleschenko <alexkv2694@gmail.com>
2022-01-14 22:54:55 +0300

    Patch

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,7 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        return cost;
+     int bonus = 20;
+     int miles = cost/bonus;
+     return miles;
     }
-
-}
+    }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
-
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        int ticketPrice = 10_000;
+//        int bonus = 20;
+//        int miles = ticketPrice/bonus;
+//        System.out.println(miles);
+//
+//
+//    }
+//}
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
+        int price = 20_000;
         int miles = service.calculate(price);
         System.out.println(miles);
     }
