ab47990f0eb5101796ecee36f657726cd09b2b14
AlexKV <bloodklesh@gmail.com>
2022-12-23 01:18:12 +0300

    update method

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 20_000;
+        int price = 10_000;
         int miles = service.calculate(price);
         System.out.println(miles);
     }
